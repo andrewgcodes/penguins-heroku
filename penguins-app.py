5b46a636a95dd1068a562d8c8520758832f81964
Andrew <olafblitz@gmail.com>
2020-08-06 11:57:03 -0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -7,38 +7,35 @@ from sklearn.ensemble import RandomForestClassifier
 st.write("""
 # Penguin Prediction App
 
-This app predicts the **Palmer Penguin** species!
-
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+Spotted a wild penguin roaming the streets? Use this web app to quickly identify the type!
 """)
 
-st.sidebar.header('User Input Features')
+st.sidebar.header('Input Features')
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+#st.sidebar.markdown("""
+#[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+#""")
 
 # Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+#uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+#if uploaded_file is not None:
+#   input_df = pd.read_csv(uploaded_file)
+def user_input_features():
+    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+    sex = st.sidebar.selectbox('Sex',('male','female'))
+    bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+    bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+    flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+    body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+    data = {'island': island,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'sex': sex}
+    features = pd.DataFrame(data, index=[0])
+    return features
+input_df = user_input_features()
 
 # Combines user input features with entire penguins dataset
 # This will be useful for the encoding phase
@@ -56,13 +53,13 @@ for col in encode:
 df = df[:1] # Selects only the first row (the user input data)
 
 # Displays the user input features
-st.subheader('User Input features')
+#st.subheader('Input features')
 
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+#if uploaded_file is not None:
+#    st.write(df)
+#else:
+#    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+#    st.write(df)
 
 # Reads in saved classification model
 load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
