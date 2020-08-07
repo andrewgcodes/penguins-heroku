52835e993191097bb8f58c192058ba6bd49f75a9
Andrew <olafblitz@gmail.com>
2020-08-07 14:00:18 -0700

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -3,75 +3,200 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use("Agg")
+from Bio.Seq import Seq 
+from Bio import SeqIO
+from collections import Counter
+import neatbio.sequtils as utils
+from PIL import Image 
 
-st.write("""
-# Penguin Prediction App
-
-Spotted a wild penguin roaming the streets? Use this web app to quickly identify the type!
-""")
-
-st.sidebar.header('Input Features')
-
-#st.sidebar.markdown("""
-#[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-#""")
-
-# Collects user input features into dataframe
-#uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-#if uploaded_file is not None:
-#   input_df = pd.read_csv(uploaded_file)
-def user_input_features():
-    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-    sex = st.sidebar.selectbox('Sex',('male','female'))
-    bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-    bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-    flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-    body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-    data = {'island': island,
-            'bill_length_mm': bill_length_mm,
-            'bill_depth_mm': bill_depth_mm,
-            'flipper_length_mm': flipper_length_mm,
-            'body_mass_g': body_mass_g,
-            'sex': sex}
-    features = pd.DataFrame(data, index=[0])
-    return features
-input_df = user_input_features()
-
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
-
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
-
-# Displays the user input features
-#st.subheader('Input features')
-
-#if uploaded_file is not None:
-#    st.write(df)
-#else:
-#    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-#    st.write(df)
-
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
-
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+def delta(x,y):
+    return 0 if x == y else 1
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+def M(seq1,seq2,i,j,k):
+    return sum(delta(x,y) for x,y in zip(seq1[i:i+k],seq2[j:j+k]))
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+
+def makeMatrix(seq1,seq2,k):
+    n = len(seq1)
+    m = len(seq2)
+    return [[M(seq1,seq2,i,j,k) for j in range(m-k+1)] for i in range(n-k+1)]
+
+
+def plotMatrix(M,t, seq1, seq2, nonblank = chr(0x25A0), blank = ' '):
+    print(' |' + seq2)
+    print('-'*(2 + len(seq2)))
+    for label,row in zip(seq1,M):
+        line = ''.join(nonblank if s < t else blank for s in row)
+        print(label + '|' + line)
+
+
+def dotplot(seq1,seq2,k = 1,t = 1):
+    M = makeMatrix(seq1,seq2,k)
+    plotMatrix(M, t, seq1,seq2) #experiment with character choice
+
+
+# Convert to Fxn
+def dotplotx(seq1,seq2):
+    plt.imshow(np.array(makeMatrix(seq1,seq2,1)))
+    # on x-axis list all sequences of seq 2
+    xt=plt.xticks(np.arange(len(list(seq2))),list(seq2))
+    # on y-axis list all sequences of seq 1
+    yt=plt.yticks(np.arange(len(list(seq1))),list(seq1))
+    plt.show()
+
+
+def gc_content(seq):
+	result = float(str(seq).count('G') + str(seq).count('C'))/len(seq) * 100
+	return result
+
+def at_content(seq):
+	result = float(str(seq).count('A') + str(seq).count('T'))/len(seq) * 100
+	return result
+
+
+
+def main():
+	"""A Simple Streamlit App """
+	st.title("BioInformatics App")
+
+	activity = ['Intro','DNA','DotPlot',"About"]
+	choice = st.sidebar.selectbox("Select Activity",activity)
+        if choice == 'Intro':
+		st.subheader("Intro")
+        elif choice == "DNA Sequence":
+		st.subheader("DNA Sequence Analysis")
+
+		seq_file = st.file_uploader("Upload FASTA File",type=["fasta","fa"])
+
+		if seq_file is not None:
+			dna_record = SeqIO.read(seq_file,"fasta")
+			# st.write(dna_record)
+			dna_seq = dna_record.seq
+
+			details = st.radio("Details",("Description","Sequence"))
+			if details == "Description":
+				st.write(dna_record.description)
+			elif details == "Sequence":
+				st.write(dna_record.seq)
+
+
+			# Nucleotide Frequencies
+			st.subheader("Nucleotide Frequency")
+			dna_freq = Counter(dna_seq)
+			st.write(dna_freq)
+			adenine_color = st.beta_color_picker("Adenine Color")
+			thymine_color = st.beta_color_picker("thymine Color")
+			guanine_color = st.beta_color_picker("Guanine Color")
+			cytosil_color = st.beta_color_picker("cytosil Color")
+
+			if st.button("Plot Freq"):
+				barlist = plt.bar(dna_freq.keys(),dna_freq.values())
+				barlist[2].set_color(adenine_color)
+				barlist[3].set_color(thymine_color)
+				barlist[1].set_color(guanine_color)
+				barlist[0].set_color(cytosil_color)
+
+				st.pyplot()
+
+			st.subheader("DNA Composition")
+			gc_score = utils.gc_content(str(dna_seq))
+			at_score = utils.at_content(str(dna_seq))
+			st.json({"GC Content":gc_score,"AT Content":at_score})
+
+			# Nucleotide Count
+			nt_count = st.text_input("Enter Nucleotide Here","Type Nucleotide Alphabet")
+			st.write("Number of {} Nucleotide is ::{}".format((nt_count),str(dna_seq).count(nt_count)))
+
+			# Protein Synthesis
+			st.subheader("Protein Synthesis")
+			p1 = dna_seq.translate()
+			aa_freq = Counter(str(p1))
+
+			if st.checkbox("Transcription"):
+				st.write(dna_seq.transcribe())
+
+			elif st.checkbox("Translation"):
+				st.write(dna_seq.translate())
+
+			elif st.checkbox("Complement"):
+				st.write(dna_seq.complement())
+
+			elif st.checkbox("AA Frequency"):
+				st.write(aa_freq)
+
+			elif st.checkbox("Plot AA Frequency"):
+				aa_color = st.beta_color_picker("Pick An Amino Acid Color")
+				# barlist = plt.bar(aa_freq.keys(),aa_freq.values(),color=aa_color)
+				# barlist[2].set_color(aa_color)
+				plt.bar(aa_freq.keys(),aa_freq.values(),color=aa_color)
+				st.pyplot()
+
+			elif st.checkbox("Full Amino Acid Name"):
+				aa_name = str(p1).replace("*","")
+				aa3 = utils.convert_1to3(aa_name)
+				st.write(aa_name)
+				st.write("=====================")
+				st.write(aa3)
+
+				st.write("=====================")
+				st.write(utils.get_acid_name(aa3))
+				
+				
+
+
+
+			# Top Most Common Amino
+
+
+
+	elif choice == "DotPlot":
+		st.subheader("Generate Dot Plot For Two Sequences")
+		seq_file1 = st.file_uploader("Upload 1st FASTA File",type=["fasta","fa"])
+		seq_file2 = st.file_uploader("Upload 2nd FASTA File",type=["fasta","fa"])
+
+		if seq_file1 and seq_file2 is not None:
+			dna_record1 = SeqIO.read(seq_file1,"fasta")
+			dna_record2 = SeqIO.read(seq_file2,"fasta")
+			# st.write(dna_record)
+			dna_seq1 = dna_record1.seq
+			dna_seq2 = dna_record2.seq
+
+			details = st.radio("Details",("Description","Sequence"))
+			if details == "Description":
+				st.write(dna_record1.description)
+				st.write("=====================")
+				st.write(dna_record2.description)
+			elif details == "Sequence":
+				st.write(dna_record1.seq)
+				st.write("=====================")
+				st.write(dna_record2.seq)
+
+
+			cus_limit = st.number_input("Select Max number of Nucleotide",10,200,50)
+			if st.button("Dot Plot"):
+				st.write("Comparing the first {} Nucleotide of the Two Sequences".format(cus_limit))
+				dotplotx(dna_seq1[0:cus_limit],dna_seq2[0:cus_limit])
+
+				st.pyplot()
+
+
+
+		
+
+
+
+	elif choice == "About":
+		st.subheader("About")
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
+        
